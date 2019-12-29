e4d0d1a7d6777dc200c9e2bda913c7c40671e2e9
Alan Wolfe <alan.wolfe@gmail.com>
2019-12-28 19:35:13 -0800

    optimize build

--- DSPFIR/build.bat
+++ DSPFIR/build.bat
@@ -1,7 +1,10 @@
 clang ^
   --target=wasm32 ^
+  -O3 ^
+  -flto ^
   -nostdlib ^
   -Wl,--no-entry ^
   -Wl,--export-all ^
+  -Wl,--lto-O3 ^
   -o hello.wasm ^
   *.c
--- DSPFIR/hello.c
+++ DSPFIR/hello.c
@@ -1,11 +1,3 @@
-/*#include <stdio.h>
-int main (int argc, char** argv)
-{
-  printf("hello world!\n");
-  return 0;
-}
-*/
-
 int add(int a, int b) {
   return a*a + b;
 }
