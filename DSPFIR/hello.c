eb8f1256d2e236d757db04f6719a5c40227a4b37
Alan Wolfe <alan.wolfe@gmail.com>
2019-12-28 20:37:08 -0800

    minor progress

--- DSPFIR/hello.c
+++ DSPFIR/hello.c
@@ -1,3 +1,19 @@
-int add(int a, int b) {
-  return a*a + b;
+typedef unsigned char byte;
+
+// TODO: could we return float maybe?
+
+static const int c_numHeights = 1024;
+
+static byte s_graphHeight[c_numHeights];
+
+byte* GetGraphHeights()
+{
+	for (int i = 0; i < c_numHeights; ++i)
+		s_graphHeight[i] = i / 4;
+	return s_graphHeight;
+}
+
+// TODO: get rid of this
+int Add(int a, int b) {
+  return a*a + b*b + a + b;
 }
